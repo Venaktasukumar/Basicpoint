dc2b8dea9cc36b01478f15db302708c1ec03d655
Venaktasukumar <malleboina.sukumar@tringapps.com>
2023-01-27 17:14:12 +0530

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,38 @@
+package org.example;
+import java.util.*;
+import java.util.logging.Logger;
+class Basicpoint implements Cloneable{
+    int x;
+    int y;
+    public Basicpoint(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
+    public Object clone() throws CloneNotSupportedException{
+        return super.clone();
+    }
+    public String eqqual(int x1, int y1){
+
+        if(x==x1 && y==y1){
+            return "true";
+        }
+        return "false";
+    }
+    public static void main(String[] args) throws CloneNotSupportedException{
+        Scanner sc=new Scanner(System.in);
+        Logger l= Logger.getLogger("com.api.jar");
+        int xaxis=sc.nextInt();
+        int yaxis=sc.nextInt();
+        int xaxis1=sc.nextInt();
+        int yaxis1=sc.nextInt();
+        l.info("Before cloning:");
+        Basicpoint bp=new Basicpoint(xaxis,yaxis);
+        String a=bp.eqqual(xaxis1,yaxis1);
+        l.info(a);
+        l.info("After cloning:");
+        Basicpoint bp1=(Basicpoint)bp.clone();
+        String b=bp1.eqqual(bp1.x,bp1.y);
+        l.info(b);
+        sc.close();
+    }
+}
